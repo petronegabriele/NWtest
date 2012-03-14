d62e0339d2fef0a987a6efb461264abedc5ee618
spilgabriele <gabrielespil@gmail.com>
2012-03-14 14:54:43 +0100

    Added plot to show graph with gnuplot

--- ab.sh
+++ ab.sh
@@ -3,10 +3,10 @@ REQUESTS=5000
 earlyDtlUrl=http://127.0.0.1:8080/wdg-profile
 ctppUrl=http://127.0.0.1/widget/profile
 
-ab -n $REQUESTS -c 10   $earlyDtlUrl > abResults/earlyDtl-$REQUESTS-10.txt 
-ab -n $REQUESTS -c 100  $earlyDtlUrl > abResults/earlyDtl-$REQUESTS-100.txt 
-ab -n $REQUESTS -c 1000 $earlyDtlUrl > abResults/earlyDtl-$REQUESTS-100.txt 
+ab -n $REQUESTS -c 10   -g abResults/earlyDtl-$REQUESTS-10.txt $earlyDtlUrl 
+ab -n $REQUESTS -c 100  -g abResults/earlyDtl-$REQUESTS-100.txt $earlyDtlUrl  
+ab -n $REQUESTS -c 1000 -g abResults/earlyDtl-$REQUESTS-1000.txt $earlyDtlUrl 
 
-ab -n $REQUESTS -c 10   $ctppUrl > abResults/ctpp-$REQUESTS-10.txt 
-ab -n $REQUESTS -c 100  $ctppUrl > abResults/ctpp-$REQUESTS-100.txt
-ab -n $REQUESTS -c 1000 $ctppUrl > abResults/ctpp-$REQUESTS-1000.txt
+ab -n $REQUESTS -c 10   -g abResults/ctpp-$REQUESTS-10.txt $ctppUrl 
+ab -n $REQUESTS -c 100  -g abResults/ctpp-$REQUESTS-100.txt $ctppUrl 
+ab -n $REQUESTS -c 1000 -g abResults/ctpp-$REQUESTS-1000.txt $ctppUrl 
---
+++ plot.p
@@ -0,0 +1,11 @@
+set terminal png
+set output "results.png"
+set title "ab -n 5000 -c 1000"
+set size 1,0.7
+
+set grid y
+set xlabel "request"
+set ylabel "response time (ms)"
+
+plot "abResults/earlyDtl-5000-1000.txt" using 9 smooth sbezier with lines title "earlyDtl", \
+"abResults/ctpp-5000-1000.txt" using 9 smooth sbezier with lines title "ctpp"
